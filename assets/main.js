3afff3b7a30ebfe579737bc93a4e2ef0dd13eef2
Caroline Trombeta <carolinecampost@gmail.com>
2023-01-19 14:18:31 +0000

    19.01 - created files of js and css

---
+++ assets/main.js
@@ -0,0 +1,38 @@
+
+const reel1 = document.getElementById("reel1");
+const reel2 = document.getElementById("reel2");
+const reel3 = document.getElementById("reel3");
+const spinButton = document.getElementById("spin-button");
+const result = document.getElementById("result");
+
+// array of symbols for the slot machine
+const symbols = [
+    "cherry",
+    "lemon",
+    "orange",
+    "plum",
+    "bell",
+    "bar",
+    "seven",
+    "blank",
+];
+
+// function to spin the reels and determine the outcome
+function spin() {
+    const spin1 = Math.floor(Math.random() * symbols.length);
+    const spin2 = Math.floor(Math.random() * symbols.length);
+    const spin3 = Math.floor(Math.random() * symbols.length);
+
+    reel1.innerHTML = symbols[spin1];
+    reel2.innerHTML = symbols[spin2];
+    reel3.innerHTML = symbols[spin3];
+
+    if (spin1 === spin2 && spin2 === spin3) {
+        result.innerHTML = "JACKPOT!";
+    } else {
+        result.innerHTML = "Try Again!";
+    }
+}
+
+// event listener for the spin button
+spinButton.addEventListener("click", spin);
